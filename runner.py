695bac0960ca13ba34d0d1f4e0e99c9b8cbf63cf
Ibraheem Awad <ibraheem_adel_awad@hotmail.com>
2023-06-03 14:02:47 +0300

    edited

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ import subprocess as sp
 class bcolors:
     HEADER = '\033[95m'
     OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
+    OKGREEN = '\033[91m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
